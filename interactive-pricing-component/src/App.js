0900c98541888e5db0f17f8dd8ba2e8931c1db0d
Nick Song <nick.senap@gmail.com>
2021-02-09 20:28:15 +0100

    wip

---
+++ interactive-pricing-component/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span class="circle1"></span>
+        <span class="circle2"></span>
+        
+         <h2> Simple, traffic-based pricing</h2>
+         <h3>  Sign-up for our 30-day trial. No credit card required.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
