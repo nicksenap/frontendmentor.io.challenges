9208f3eceaabfdb7a62f343361162e09c7085f4d
Nick Song <nick.senap@gmail.com>
2021-03-11 10:05:56 +0100

    Card Component wip

---
+++ rest-countries-api-with-color-theme-switcher-master/src/components/molecules/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <img :src="countryData.flag" />
+    <div class="card__desc">
+      <h2>{{ countryData.name }}</h2>
+      <h3>Population: {{ countryData.population }}</h3>
+      <h3>Region: {{ countryData.region }}</h3>
+      <h3>Capital: {{ countryData.capital }}</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Country } from '@/types/country.interface';
+  import { defineComponent, PropType } from 'vue';
+  import { mapState, useStore } from 'vuex';
+  export default defineComponent({
+    setup() {
+      const store = useStore();
+      return { store };
+    },
+    computed: {
+      ...mapState(['isDarkTheme'])
+    },
+    props: { countryData: { type: Object as PropType<Country>, required: true } }
+  });
+</script>
+
+<style></style>
---
+++ rest-countries-api-with-color-theme-switcher-master/src/components/organisms/CardContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <card v-for="" />
+    <card :countryData="store.state.countries[0]" />
+    <card :countryData="store.state.countries[1]" />
+    <card :countryData="store.state.countries[2]" />
+    <card :countryData="store.state.countries[3]" />
+  </div>
+</template>
+
+<script lang="ts">
+  import Card from '../molecules/Card.vue';
+  import { defineComponent } from 'vue';
+  import { mapState, useStore } from 'vuex';
+  export default defineComponent({
+    components: {
+      Card
+    },
+    setup() {
+      const store = useStore();
+      return { store };
+    },
+    props: { countryData: { type: Object as Array<Country>, required: true } },
+    computed: {
+      ...mapState(['isDarkTheme'])
+    }
+  });
+</script>
+
+<style></style>
